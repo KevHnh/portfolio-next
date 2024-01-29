d1735e8cb06d5495111730ae341ab311b8899fa6
KevHnh <kevinhinh9@gmail.com>
2024-01-29 02:19:18 -0500

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
-  title: "Kevin Hinh",
+  title: "Kevin Hinh | SWE & Designer",
   description: "Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
